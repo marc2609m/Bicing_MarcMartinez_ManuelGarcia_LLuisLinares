12648712ecbc64e80d3407aaa2e98d30b8b44667
marc2609m <marcmm2609@gmail.com>
2024-02-02 16:16:13 +0100

    Proyecto Vacio

---
+++ app/src/main/java/com/example/bicing_marcmartinez_manuelgarcia_lluisartes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bicing_marcmartinez_manuelgarcia_lluisartes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Bicing_MarcMartinez_ManuelGarcia_LLuisArtes"
+include(":app")
+ 
